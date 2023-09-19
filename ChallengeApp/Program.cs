a3462693750570c3a444d967dfd299d05a9e76a1
Tomasz Krajewski <enderksenobojca@gmail.com>
2023-09-19 16:08:06 +0200

    zadanie domowe dzień 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var name = "Ewa";
+var sex = "woman";
+var age = 33;
+if (sex == "woman" && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (sex != "woman" && age < 18)  //staromodnie zakładam, że sa 2 płcie
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (name == "Ewa" && age == 33 && sex == "woman")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else
+{
+    Console.WriteLine("sekwencja autodestrukcji uruchomiona");
+}
